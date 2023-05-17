9ffab08ab5cc1a348633a2bdb67861b21c1acd04
xyt417 <417639959@qq.com>
2023-05-17 21:10:17 +0800

    2

---
+++ Template/数据结构/单链表.cpp
@@ -0,0 +1,15 @@
+const int N = 100010;
+int head, e[N], ne[N], idx;
+// 初始化
+void init(){
+    head = -1;
+    idx = 0;
+}
+// 头插
+void insert(int a){
+    e[idx] = a, ne[idx] = head, head = idx ++;
+}
+// 头删
+void remove(){
+    head = ne[head];
+}
---
+++ Template/数据结构/双链表.cpp
@@ -0,0 +1,18 @@
+const int N = 100010;
+int e[N], l[N], r[N], idx;
+// 初始化
+void init(){
+    r[0] = 1, l[1] = 0;
+    idx = 2;
+}
+// 在节点a右端插入值
+void insert(int a, int x){
+    e[idx] = x;
+    l[idx] = a, r[idx] = r[a]; 
+    l[r[a]] = idx, r[a] = idx ++;
+}
+// 删除节点a
+void remove(int a){
+    l[r[a]] = l[a];
+    r[l[a]] = r[a];
+}
