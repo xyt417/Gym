2f2bf8da74fd74702751203cbf99285a71d523fb
xyt417 <417639959@qq.com>
2023-05-17 20:19:51 +0800

    Update 区间合并.cpp

--- Template/区间合并.cpp
+++ Template/区间合并.cpp
@@ -2,11 +2,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef pair<int, int> PII;
-void merge(vector<PII> &segs){
+const int _inf = -2e9;
+int n;
+vector<PII> merge(vector<PII> &segs){
+    vector<PII> res;
     sort(segs.begin(), segs.end());
-    int st = 2e-9, ed = 2e-9;
+    int st = _inf, ed = _inf;
+    for(PII seg : segs){
+        if(ed < seg.first){
+            if(st != _inf) res.push_back({st, ed});
+            st = seg.first, ed = seg.second;
+        }
+        else ed = max(ed, seg.second);
+    }
+    if(st != _inf) res.push_back({st, ed});
+    return res;
 }
 int main(){
-
+    cin >> n;
+    vector<PII> segs;
+    while(n --){
+        PII seg;
+        cin >> seg.first >> seg.second;
+        segs.push_back(seg);
+    }
+    // segs = merge(segs);
+    // for(PII seg : segs){
+    //     cout << "[" << seg.first << ',' << seg.second << "]" << '\n';
+    // }
+    cout << merge(segs).size();
     return 0;
 }
