77f8d902429f02910c21224ebe6274ef049ea2ce
xyt417 <417639959@qq.com>
2023-05-20 20:10:17 +0800

    Prim

--- Template/搜索与图论/Prim.cpp
+++ Template/搜索与图论/Prim.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int N = 1010, INF = 0x3f3f3f3f;
+
 int n, m;
 int g[N][N];
 int dist[N];
@@ -8,29 +9,30 @@ bool st[N];
 int prim(){
     memset(dist, 0x3f, sizeof(dist));
     int res = 0;
-    dist[1] = 0;
-    for(int i = 1; i <= n; ++ i){
+    dist[1] = 0, st[1] = true;
+    for(int i = 1; i < n; ++ i){
         int t = -1;
         for(int j = 1; j <= n; ++ j)
-            if(!st[j] && (t == -1 || dist[j] < dist[t]))
+            if(!st[j] && (t == -1 || dist[j] < dist[t])){
                 t = j;
+                cout << t << endl;
+            }
         if(dist[t] == INF) return INF;
-        res += dist[t]; 
-        // 先加结果后更新 自环可能更新自己
         for(int j = 1; j <= n; ++j)
             dist[j] = min(dist[j], g[t][j]);
+        res += dist[t];
         st[t] = true;
     }
     return res;
 }
-// [Prim求最小生成树]
+
 int main(){
     cin >> n >> m;
     memset(g, 0x3f, sizeof(g));
     while(m --){
         int x, y, z;
         cin >> x >> y >> z;
-        g[x][y] = g[y][x] = min(g[x][y], z);
+        g[x][y] = min(g[x][y], z);
     }
     int res = prim();
     if(res == INF) cout << "impossible";
